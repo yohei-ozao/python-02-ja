e6bf3c468b9077fcc568b1fed93340efaff3835f
yohei-ozao_stargate <yohei.ozao@woven-planet.global>
2024-08-23 18:43:36 +0900

    add apply_discount.py

--- 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
+++ 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
@@ -1,2 +1,15 @@
-def apply_discount(products, minimum_price, discount_rate):
-    pass
+def apply_discount(
+    products,
+    minimum_price,
+    discount_rate,
+):
+    calculate_price = lambda price: price * (1 - discount_rate * 0.01)
+
+    return [
+        (
+            elem["name"],
+            calculate_price(elem["price"]),
+        )
+        for elem in products
+        if elem["price"] >= minimum_price
+    ]
